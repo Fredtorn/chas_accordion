a6885dead6cd5d351d7a735e116661cbcf13f10d
Fredtorn <fredrika.tornkvist@chasacademy.se>
2023-11-15 15:58:24 +0100

    new code

--- code/script.js
+++ code/script.js
@@ -4,6 +4,50 @@ function toggle(e) {
   element.classList.toggle("active");
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-section1Element.addEventListener("click", toggle);
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+
+
+
+for (i = 1; i <= 3; i++) {
+  const sectionEl = document.getElementById(`section${i}`)
+  sectionEl.addEventListener("click", toggle)
+}
+
+const accordionEL = document.getElementById("accordion");
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const filteredData = data.filter((posts) => posts.userId === 2);
+
+    filteredData.forEach((posts) => {
+
+      const titleEl = document.createElement("div");
+      const bodyEl = document.createElement("div");
+      const pEl = document.createElement("p");
+      const textNode = document.createTextNode(posts.body);
+
+
+      titleEl.setAttribute("class", "title");
+      titleEl.setAttribute("id", `section${posts.id}`)
+      bodyEl.setAttribute("class", "description");
+
+
+      titleEl.innerHTML = `${posts.title}`;
+
+      accordionEL.appendChild(titleEl);
+      pEl.appendChild(textNode)
+      bodyEl.appendChild(pEl);
+      accordionEL.appendChild(bodyEl)
+
+      const sectionElement = document.getElementById(`section${posts.id}`)
+      sectionElement.addEventListener("click", toggle)
+
+      console.log(sectionElement)
+    })
+
+
+  });
+
